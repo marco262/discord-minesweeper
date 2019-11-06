8a541ae0f4f1a4fbb31eb1cb4a7eb2bdc70b5618
marco262 <marco262@gmail.com>
2019-11-06 12:18:28 -0800

    Create emoji_parser.py

---
+++ emoji_parser.py
@@ -0,0 +1,18 @@
+# ||:one:|| ||:two:|| ||:three:|| ||:four:|| ||:five:|| ||:six:|| ||:seven:|| ||:eight:|| ||:bomb:|| ||:white_large_square:||
+
+emoji_dict = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    0: "white_large_square",
+    "B": "boom"
+}
+
+
+def grid_to_emoji(grid):
+    print("\n".join([" ".join([f"||:{emoji_dict[c]}:||" for c in row]) for row in grid]))
