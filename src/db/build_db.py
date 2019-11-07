9335b1ad6fad292feadab26ded5044d74655c43f
marco262 <marco262@gmail.com>
2019-11-07 08:10:52 -0800

    Rename user to owner

--- src/db/build_db.py
+++ src/db/build_db.py
@@ -16,7 +16,7 @@ DROP TABLE IF EXISTS tasks;
 print(cur.execute(sql).fetchall())
 
 sql = """
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS owners (
     id INTEGER PRIMARY KEY, 
     name TEXT NOT NULL, 
     creation_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -28,6 +28,7 @@ sql = """
 CREATE TABLE IF NOT EXISTS tasks (
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
     name TEXT NOT NULL, 
+    owner_id TEXT NOT NULL,
     creation_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     started_ts TIMESTAMP DEFAULT NULL,
     completed_ts TIMESTAMP DEFAULT NULL
@@ -35,5 +36,5 @@ CREATE TABLE IF NOT EXISTS tasks (
 """
 print(cur.execute(sql).fetchall())
 
-print(cur.execute("select sql from sqlite_master where type = 'table' and name = 'users';").fetchone()[0])
+print(cur.execute("select sql from sqlite_master where type = 'table' and name = 'owners';").fetchone()[0])
 print(cur.execute("select sql from sqlite_master where type = 'table' and name = 'tasks';").fetchone()[0])
--- src/db/db.py
+++ src/db/db.py
@@ -23,20 +23,15 @@ class Db:
             finally:
                 self.db.close()
 
-    def add_user(self, discord_id, user):
-        sql = "INSERT OR IGNORE INTO users (id, name) VALUES (?, ?)"
+    def add_owner(self, discord_id, user):
+        sql = "INSERT OR IGNORE INTO owners (id, name) VALUES (?, ?)"
         print(self.cur.execute(sql, [discord_id, user]).fetchall())
 
-    def list_users(self):
-        sql = "SELECT * FROM users"
+    def list_owners(self):
+        sql = "SELECT * FROM owners"
         print(self.cur.execute(sql).fetchall())
 
-    def get_user_id(self, name):
-        sql = "SELECT id FROM users WHERE name=?"
+    def get_owner_id(self, name):
+        sql = "SELECT id FROM owners WHERE name=?"
         return self.cur.execute(sql, [name]).fetchone()[0]
 
-
-if __name__ == "__main__":
-    with Db() as db:
-        db.add_user(1234567, "Marco262")
-        print(db.get_user_id("Marco262"))
