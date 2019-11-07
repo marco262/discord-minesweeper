7e2b5eb88c51e8ca228d229c7a4652c60b2eaab1
marco262 <marco262@gmail.com>
2019-11-07 07:04:40 -0800

    Added tasks table

--- build_db.py
+++ build_db.py
@@ -3,18 +3,30 @@ import sqlite3
 db = sqlite3.connect("list-keeper.db")
 cur = db.cursor()
 
-cur.execute("""
-    DROP TABLE IF EXISTS users;
-""")
-print(cur.fetchall())
+sql = """
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS tasks;
+"""
+print(cur.execute(sql).fetchall())
 
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY, 
-        name TEXT NOT NULL, 
-        creation_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
-""")
-print(cur.fetchall())
+sql = """
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY, 
+    name TEXT NOT NULL, 
+    creation_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+"""
+print(cur.execute(sql).fetchall())
 
-print(cur.execute("select sql from sqlite_master where type = 'table' and name = 'users';").fetchone()[0])
+sql = """
+CREATE TABLE IF NOT EXISTS tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    name TEXT NOT NULL, 
+    creation_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    started_ts TIMESTAMP DEFAULT NULL,
+    completed_ts TIMESTAMP DEFAULT NULL
+);
+"""
+print(cur.execute(sql).fetchall())
+
+# print(cur.execute("select sql from sqlite_master where type = 'table' and name = 'users';").fetchone()[0])
