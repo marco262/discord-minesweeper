88628c4028e8da56d04bd242c3e1faffbe2b4b39
marco262 <marco262@gmail.com>
2019-11-09 13:18:37 -0800

    Rearranged build_db

--- src/db/build_db.py
+++ src/db/build_db.py
@@ -1,56 +1,71 @@
 import os
 import sqlite3
 
-os.makedirs("../../database_files/", exist_ok=True)
-db = sqlite3.connect("../../database_files/list-keeper.db")
-cur = db.cursor()
 
-sql = """
-DROP TABLE IF EXISTS users;
-"""
-print(cur.execute(sql).fetchall())
-
-sql = """
-DROP TABLE IF EXISTS tasks;
-"""
-print(cur.execute(sql).fetchall())
-
-sql = """
-DROP TABLE IF EXISTS lists;
-"""
-print(cur.execute(sql).fetchall())
-
-sql = """
-CREATE TABLE IF NOT EXISTS owners (
-    id INTEGER PRIMARY KEY, 
-    name TEXT NOT NULL, 
-    created_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-"""
-print(cur.execute(sql).fetchall())
-
-sql = """
-CREATE TABLE IF NOT EXISTS tasks (
-    name TEXT NOT NULL, 
-    owner_id INTEGER NOT NULL,
-    created_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    started_ts TIMESTAMP DEFAULT NULL,
-    completed_ts TIMESTAMP DEFAULT NULL
-);
-"""
-print(cur.execute(sql).fetchall())
-
-sql = """
-CREATE TABLE IF NOT EXISTS lists (
-    items TEXT NOT NULL,
-    owner_id INTEGER NOT NULL UNIQUE,
-    created_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-"""
-print(cur.execute(sql).fetchall())
+def open_db():
+    os.makedirs("../../database_files/", exist_ok=True)
+    db = sqlite3.connect("../../database_files/list-keeper.db")
+    cur = db.cursor()
+    return db, cur
 
 
-print(cur.execute("select sql from sqlite_master where type = 'table' and name = 'owners';").fetchone()[0])
-print(cur.execute("select sql from sqlite_master where type = 'table' and name = 'tasks';").fetchone()[0])
-print(cur.execute("select sql from sqlite_master where type = 'table' and name = 'lists';").fetchone()[0])
+def drop_tables(cur):
+    sql = """
+    DROP TABLE IF EXISTS users;
+    """
+    print(cur.execute(sql).fetchall())
+
+    sql = """
+    DROP TABLE IF EXISTS tasks;
+    """
+    print(cur.execute(sql).fetchall())
+
+    sql = """
+    DROP TABLE IF EXISTS lists;
+    """
+    print(cur.execute(sql).fetchall())
+
+
+def create_tables(cur):
+    sql = """
+    CREATE TABLE IF NOT EXISTS owners (
+        id INTEGER PRIMARY KEY, 
+        name TEXT NOT NULL, 
+        created_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    """
+    print(cur.execute(sql).fetchall())
+
+    sql = """
+    CREATE TABLE IF NOT EXISTS tasks (
+        name TEXT NOT NULL, 
+        owner_id INTEGER NOT NULL,
+        created_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        started_ts TIMESTAMP DEFAULT NULL,
+        completed_ts TIMESTAMP DEFAULT NULL,
+        time_spent_sec INTEGER DEFAULT 0
+    );
+    """
+    print(cur.execute(sql).fetchall())
+
+    sql = """
+    CREATE TABLE IF NOT EXISTS lists (
+        items TEXT NOT NULL,
+        owner_id INTEGER NOT NULL UNIQUE,
+        created_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    """
+    print(cur.execute(sql).fetchall())
+
+    print(cur.execute("select sql from sqlite_master where type = 'table' and name = 'owners';").fetchone()[0])
+    print(cur.execute("select sql from sqlite_master where type = 'table' and name = 'tasks';").fetchone()[0])
+    print(cur.execute("select sql from sqlite_master where type = 'table' and name = 'lists';").fetchone()[0])
+
+
+if __name__ == "__main__":
+    db, cur = open_db()
+    drop_tables(cur)
+    create_tables(cur)
+    db.commit()
+    db.close()
